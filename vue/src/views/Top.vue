f00981947c8eabd025dad881d111db7778948a7e
mihohiroshima <miho.hiroshima@seattleconsulting.co.jp>
2020-05-11 17:35:36 +0900

    TORIAEZU

--- vue/src/views/Top.vue
+++ vue/src/views/Top.vue
@@ -6,3 +6,13 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import { Line } from 'vue-chartjs';
+import Chart from './Chart';
+export default {
+  components:{
+    Chart
+  }
+}
+</script>
