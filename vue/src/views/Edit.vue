9366747470c19cb018e5ab0793ac6439c7d1943c
takahiro-kaji-1122 <takahiro.kaji@seattleconsulting.co.jp>
2020-05-12 16:06:00 +0900

    入力箇所作成

--- vue/src/views/Edit.vue
+++ vue/src/views/Edit.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="edit">
-    <h1>This is an Edit page</h1>
+  <div>
+    <form action="/my-handling-form-page" method="post">
+      <ul>
+        <li>
+          <label for="age">年齢</label>
+          <input id="age" type="int" name="age">
+        </li>
+        <li>
+          <label for="score">スコア</label>
+          <input id="score" type="int" name="score">
+        </li>
+        <li>
+          <label for="comment">コメント</label>
+          <input id="comment" type="varchar(255)" name="comment">
+        </li>
+      </ul>
+      <button type="submit">
+        登録
+      </button>
+      <button type="submit">
+        編集
+      </button>
+    </form>
+    <button onclick="location.href='./">
+      遷移
+    </button>
   </div>
 </template>
+
+<style>
+ul {
+  list-style: none;
+}
+</style>
