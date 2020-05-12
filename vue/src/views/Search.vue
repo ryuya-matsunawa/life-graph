2d6c67536af24f3600dd5ddaaaab7a633cd563a9
eisuke-hayashi <email>
2020-05-12 11:06:57 +0900

    最新化

--- vue/src/views/Search.vue
+++ vue/src/views/Search.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="search">
-    <ul class="searchList">
-      <li>
-        aaaa
-      </li>
-      <li>
-        bbbb
-      </li>
-      <li>
-        cccc
-      </li>
-    </ul>
-    <button>参照</button>
-  </div>
+  <div class="form-container container" id="app">
+  <form>
+    <div class="form-group">
+      <label for="exampleInputEmail1">ユーザー名</label>
+      <div class="form-row">
+        <div class="col">
+          <input v-model="form.lastName" type="text" class="form-control" placeholder="名前">
+          <small v-bind:class="{'is-hide':validation.lastName}"  class="form-text text-info">{{ errorMessage.lastName }}</small>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputEmail1">更新日時</label>
+      <input v-model="form.tel" type="tel" class="form-control" id="exampleInputEmail1" placeholder="電話番号">
+      <small v-bind:class="{'is-hide':validation.tel}" class="form-text text-info">{{ errorMessage.tel }}</small>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputEmail1">表示順</label>
+      <input v-model="form.email" type="email" class="form-control" id="exampleInputEmail1" placeholder="メールアドレス">
+      <small v-bind:class="{'is-hide':validation.email}" class="form-text text-info">{{ errorMessage.email }}</small>
+    </div>
+    <div class="btn-container"><button type="submit" v-bind:disabled="!isValid" class="btn btn-primary">参照</button></div>
+  </form>
+</div>
 </template>
 
 <script>
