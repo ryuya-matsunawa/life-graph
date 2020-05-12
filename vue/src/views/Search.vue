fedffac0375d8c30422de0378431272667550fbd
eisuke-hayashi <email>
2020-05-12 10:40:34 +0900

    ブランチ最新化

--- vue/src/views/Search.vue
+++ vue/src/views/Search.vue
@@ -1,46 +1,55 @@
 <template>
-  <div class="scene1">
-    <p class="name">NICKNAME: {{ nickname }}</p>
-    <input class="input" v-model="nickname" placeholder="NICKNAMEを入力してください。">
-    <router-link to="/scene2" class="button">検索</router-link>
+  <div class="search">
+    <ul class="searchList">
+      <li>
+        aaaa
+      </li>
+      <li>
+        bbbb
+      </li>
+      <li>
+        cccc
+      </li>
+    </ul>
+    <button>参照</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'nickname',
+  name: 'Search',
   data () {
     return {
-      nickname: ''
+      search: ''
     }
   }
 }
 </script>
 
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-.scene1{
-  font-size: 1.8rem;
-  text-align: center;
-  a div p{
-    display: block;
-    margin: 0 auto;
-    height:30pt;
-  }
-}
-.input{
-  width: 130pt;
-  height:30pt;
-}
-.button {
-  display: block;
-  position: relative;
-  margin: 0 auto;
-  width: 70pt;
-  border: solid 1px silver;
-  border-radius: 0.5rem 0.5rem;
-  padding: 0.5rem 1.5rem;
-  margin-top: 1rem;
-  text-decoration: none;
-}
-</style>
+// <style scoped lang="scss">
+// .scene1{
+//   font-size: 1.8rem;
+//   text-align: center;
+//   a div p{
+//     display: block;
+//     margin: 0 auto;
+//     height:30pt;
+//   }
+// }
+// .input{
+//   width: 130pt;
+//   height:30pt;
+// }
+// .button {
+//   display: block;
+//   position: relative;
+//   margin: 0 auto;
+//   width: 70pt;
+//   border: solid 1px silver;
+//   border-radius: 0.5rem 0.5rem;
+//   padding: 0.5rem 1.5rem;
+//   margin-top: 1rem;
+//   text-decoration: none;
+// }
+// </style>
