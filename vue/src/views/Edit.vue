027e20a7a58475547d5795a474174908d4cd1312
takahiro-kaji-1122 <takahiro.kaji@seattleconsulting.co.jp>
2020-05-12 16:58:57 +0900

    チャート・ヘッダー乗せた

--- vue/src/views/Edit.vue
+++ vue/src/views/Edit.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
+    <Header />
     <form action="/my-handling-form-page" method="post">
       <ul>
         <li>
-          <label for="age">年齢</label>
+          <label class=tag for="age">年齢</label>
           <input id="age" type="int" name="age">
         </li>
         <li>
-          <label for="score">スコア</label>
+          <label class=tag for="score">スコア</label>
           <input id="score" type="int" name="score">
         </li>
         <li>
-          <label for="comment">コメント</label>
+          <label class=tag for="comment">コメント</label>
           <input id="comment" type="varchar(255)" name="comment">
         </li>
       </ul>
@@ -25,11 +26,33 @@
     <button onclick="location.href='./">
       遷移
     </button>
+    <div class="chart">
+      <Chart />
+    </div>
   </div>
 </template>
+
+<script>
+import Chart from '../views/Chart.vue'
+import Header from '../views/Header.vue'
+export default {
+  name: 'Top',
+  components: {
+    Chart,
+    Header
+  }
+}
+</script>
 
 <style>
 ul {
   list-style: none;
 }
+label{
+  width: 40;
+}
+.tag{
+  width: 40;
+}
+
 </style>
