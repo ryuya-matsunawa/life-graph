f23429734a40a4946cc010b98b3fd1800db42ceb
mihohiroshima <miho.hiroshima@seattleconsulting.co.jp>
2020-05-13 16:39:41 +0900

    TOPのモック完成

--- vue/src/views/Chart.vue
+++ vue/src/views/Chart.vue
@@ -1,5 +1,6 @@
 <script>
 import { Line } from 'vue-chartjs'
+// import { mapMutations } from 'vuex'
 
 export default {
   name: 'Chart',
@@ -8,13 +9,11 @@ export default {
     return {
       data: {
         // 年齢
-        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25'],
+        labels: [],
         datasets: [
           {
             // スコア
-            data: [0, 30, 30, 40, -20, 50, 50, 60, 70, 0,
-              0, 2, 20, 40, 22, 0, 8, 0, 2, 2,
-              70, 70, 70, 70, 0],
+            data: [],
             borderColor: '#CFD8DC',
             // 線の中に色つけるかどうか
             fill: false,
@@ -62,7 +61,19 @@ export default {
     }
   },
   mounted () {
+    this.getAge()
+    this.getLifeScores()
     this.renderChart(this.data, this.options)
+  },
+  methods: {
+    getAge () {
+      const age = this.$store.state.age
+      this.data.labels = age
+    },
+    getLifeScores () {
+      const lifeScores = this.$store.state.lifeScores
+      this.data.datasets[0].data = lifeScores
+    }
   }
 }
 </script>
