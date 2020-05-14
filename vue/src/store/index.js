9b070c78f08c210057b63d091e62e860690f592b
takahiro-kaji-1122 <takahiro.kaji@seattleconsulting.co.jp>
2020-05-14 11:04:18 +0900

    一旦

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import chart from './modules/chart.js'
-import account from './modules/account.js.js'
+import account from './modules/account.js'
 
 Vue.use(Vuex)
 
