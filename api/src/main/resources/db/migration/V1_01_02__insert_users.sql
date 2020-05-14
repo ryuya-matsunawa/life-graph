f5ad89e4a52e84408b832cbd06c3addb1d9d32be
松縄竜弥 <ryuya.matsunawa@seattleconsulting.co.jp>
2020-05-14 15:29:43 +0900

    add:login_api

---
+++ api/src/main/resources/db/migration/V1_01_02__insert_users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(
+`mail_address`,`name`,`password`)VALUES
+('manato@example.com','manato','password'),
+('ryuya@example.com','ryuya','password'),
+('eisuke@example.com','eisuke','password'),
+('kaji@example.com','kaji','password');
