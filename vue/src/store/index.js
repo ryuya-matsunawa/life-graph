4d56246de8828d9aa271c30a2ec06517cb879992
eisuke-hayashi <email>
2020-05-18 10:13:58 +0900

    検索モック画面

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import chart from './modules/chart.js'
 import account from './modules/account.js'
+import search from './modules/search.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     chart,
-    account
+    account,
+    search
   }
 })
