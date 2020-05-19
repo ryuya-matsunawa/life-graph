48499892bc1e907599e7c5cef910b72e235fa649
eisuke-hayashi <email>
2020-05-19 18:17:54 +0900

    ユーザー検索

--- api/src/main/java/com/lifegraph/team20/controller/LifeGraphsContorller.java
+++ api/src/main/java/com/lifegraph/team20/controller/LifeGraphsContorller.java
@@ -1,6 +1,5 @@
 package com.lifegraph.team20.controller;
 
-<<<<<<< HEAD
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -9,19 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.CrossOrigin;
-=======
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
->>>>>>> develop
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
+import com.lifegraph.team20.models.Content;
 import com.lifegraph.team20.models.UserData;
+import com.lifegraph.team20.service.ContentService;
+
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -45,15 +41,7 @@ public class LifeGraphsContorller {
 			}
 		});
 	}
-}
-=======
-import com.lifegraph.team20.models.Content;
-import com.lifegraph.team20.service.ContentService;
-
-@RestController
-@RequestMapping("/life-graph")
-public class LifeGraphsContorller {
-    @Autowired
+	@Autowired
     ContentService contentService;
 
     @RequestMapping(value = "/new",method = RequestMethod.POST)
@@ -76,6 +64,36 @@ public class LifeGraphsContorller {
     void deleteContent(@PathVariable("id") Integer id) {
     	contentService.deleteContent(id);
     }
+}
+//import com.lifegraph.team20.models.Content;
+//import com.lifegraph.team20.service.ContentService;
+//
+//@RestController
+//@RequestMapping("/life-graph")
+//public class LifeGraphsContorller {
+//    @Autowired
+//    ContentService contentService;
+//
+//    @RequestMapping(value = "/new",method = RequestMethod.POST)
+//    Content postContent(@RequestBody Content content) {
+//        return contentService.postContent(content);
+//    }
+//
+//    @RequestMapping(value = "/a", method = RequestMethod.GET)
+//    List<Content> getContent() {
+//        return contentService.getContent();
+//    }
+//
+//    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+//    Content putTweet(@PathVariable("id") Integer id, @RequestBody Content content) {
+//    	content.setId(id);
+//        return contentService.updateContent(content);
+//    }
+//
+//    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+//    void deleteContent(@PathVariable("id") Integer id) {
+//    	contentService.deleteContent(id);
+//    }
 //    @RequestMapping(value = "/sample", method = RequestMethod.GET)
 //	public List<UserData> userData() {
 //		List<UserData> userDatas = setUserData();
@@ -90,5 +108,4 @@ public class LifeGraphsContorller {
 //			}
 //		});
 //	}
-}
->>>>>>> develop
+//}
