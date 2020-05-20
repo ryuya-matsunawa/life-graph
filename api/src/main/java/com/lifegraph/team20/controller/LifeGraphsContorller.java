eb63c5ef74df4fdefe8def25932f8ece4ce484d7
takahiro-kaji-1122 <takahiro.kaji@seattleconsulting.co.jp>
2020-05-20 10:57:29 +0900

    コンプリ解消途中

--- api/src/main/java/com/lifegraph/team20/controller/LifeGraphsContorller.java
+++ api/src/main/java/com/lifegraph/team20/controller/LifeGraphsContorller.java
@@ -8,13 +8,10 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-=======
->>>>>>> develop
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,24 +19,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
+import com.lifegraph.team20.models.Content;
 import com.lifegraph.team20.models.LifeGraph;
 //登録編集APIで使ってます。
 import com.lifegraph.team20.models.LifeGraphData;
-
-=======
-import com.lifegraph.team20.models.Content;
-import com.lifegraph.team20.models.LifeGraph;
-import com.lifegraph.team20.models.UserData;
-import com.lifegraph.team20.repository.LifeGraphsRepository;
-import com.lifegraph.team20.service.ContentService;
->>>>>>> develop
+import com.lifegraph.team20.service.ExistenceCheckService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/life-graphs")
 public class LifeGraphsContorller {
-<<<<<<< HEAD
 //@RequestMapping(value = "/search", method = RequestMethod.GET)
 //	public List<UserData> userData() {
 //		List<UserData> userDatas = setUserData();
@@ -166,62 +155,6 @@ public class LifeGraphsContorller {
 
 
 
-//
-//	編集したりデリートしたり。
-//	@Autowired
-//    ContentService contentService;
-//
-//	  @RequestMapping(value = "/new",method = RequestMethod.POST)
-//    ResponseEntity<Content> postContent(@RequestBody Content content) {
-//    	return ResponseEntity;
-//    }
-//
-//    @RequestMapping(value = "/a", method = RequestMethod.GET)
-//    List<Content> getContent() {
-//        return contentService.getContent();
-//    }
-//
-//    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-//    ResponseEntity<Content> putContent(@PathVariable("id") Integer id, @RequestBody Content content) {
-//    	content.setId(id);
-//
-//        return ResponseEntity.done();
-//    }
-//デリート
-//    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-//    void deleteContent(@PathVariable("id") Integer id) {
-//    	contentService.deleteContent(id);
-//    }
-}
-//import com.lifegraph.team20.models.Content;
-//import com.lifegraph.team20.service.ContentService;
-//
-//@RestController
-//@RequestMapping("/life-graph")
-//public class LifeGraphsContorller {
-//    @Autowired
-//    ContentService contentService;
-//
-//    @RequestMapping(value = "/new",method = RequestMethod.POST)
-//    Content postContent(@RequestBody Content content) {
-//        return contentService.postContent(content);
-//    }
-//
-//    @RequestMapping(value = "/a", method = RequestMethod.GET)
-//    List<Content> getContent() {
-//        return contentService.getContent();
-//    }
-//
-//    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-//    Content putTweet(@PathVariable("id") Integer id, @RequestBody Content content) {
-//    	content.setId(id);
-//        return contentService.updateContent(content);
-//    }
-//
-//    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-//    void deleteContent(@PathVariable("id") Integer id) {
-//    	contentService.deleteContent(id);
-//    }
 //    @RequestMapping(value = "/sample", method = RequestMethod.GET)
 //	public List<UserData> userData() {
 //		List<UserData> userDatas = setUserData();
@@ -237,7 +170,6 @@ public class LifeGraphsContorller {
 //		});
 //	}
 //}
-=======
 
 	@Autowired
 	LifeGraphsRepository lifeGraphsRepository;
@@ -284,4 +216,3 @@ public class LifeGraphsContorller {
 		return ResponseEntity.ok(graph);
 	}
 }
->>>>>>> develop
