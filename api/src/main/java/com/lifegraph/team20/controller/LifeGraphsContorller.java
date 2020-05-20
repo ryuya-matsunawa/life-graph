23a2c62e09bb5f057e206e4cd6ef1c585365b7f7
takahiro-kaji-1122 <takahiro.kaji@seattleconsulting.co.jp>
2020-05-20 10:32:59 +0900

    一旦

--- api/src/main/java/com/lifegraph/team20/controller/LifeGraphsContorller.java
+++ api/src/main/java/com/lifegraph/team20/controller/LifeGraphsContorller.java
@@ -4,6 +4,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.RowMapper;
@@ -12,6 +14,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,6 +57,7 @@ public class LifeGraphsContorller {
 	}
 
 	@Autowired
+	//MySQLのデータを持ってくるライブラリ
 	private NamedParameterJdbcTemplate jdbcTemplate;
 
 	private List<LifeGraph> setGraph(Integer id) {
@@ -74,8 +78,84 @@ public class LifeGraphsContorller {
 		});
 		return result;
 	}
+
+//	登録編集画面の参考：斎藤さん
+
+	@RestController
+
+	public class LifeGraphsController {
+
+	  @RequestMapping(value = "/auth/life_graphs", method = RequestMethod.POST)
+	  //  @PostMapping("/life-graphs")
+	  //Validでバリデーションチェックしてくれてる。
+	  //  public ResponseEntity<String> postController(@Valid @RequestBody LifeGraphData data) {
+	  public ResponseEntity<String> postController(@Valid @RequestBody LifeGraphData data) {
+	    //    LifeGraphData lifegraph = data;
+
+	    //    LifeGraphData lifegraph = data;
+
+	    long id = data.getUserId();
+	    int ageInt = data.getAge();
+	    int scoreInt = data.getScore();
+	    String comment = data.getComment();
+
+	    //存在チェック
+	    ExistenceCheckService.exists(id, ageInt);
+
+	    //    return ResponseEntity.ok("OK");
+	    return ResponseEntity.ok("OK");
+	  }
+
+	}
+
+//	【登録編集API】
+
+//	＜仕様書のURI＞
+//	人生グラフ登録(編集)API
+//	モック作成
+//	登録：{画面URL}/life-graph/new
+//	編集：{画面URL}/life-graph/edit/{id}
+//	人生グラフ登録(編集)API
+//	POST：{API URL}/life-graphs
+//	人生グラフ参照API
+//	GET：{API URL}/life-graphs/{id}
+
+
+//	【登録APIを作りたい】
 //	追加登録できるやつを作りたい
-//	参照APIとの差はなに？=>多分
+//	参照APIとの差はなに？=>多分sqlをインサートにして、param引数をidで、エンティティにフォームからのものを入れる。
+    @RequestMapping(value = "/new", method = RequestMethod.POST)
+//  Entityクラスとは、dbで扱うデータをアプリケーションで保持するための入れ物のようなものです。
+//  value = "/{id}"のidがInteger idに入る
+	public ResponseEntity<List<LifeGraph>> graph(@PathVariable("id") Integer id) {
+//  	下のidを引数として渡す（上のInteger idのこと）
+//  	setGraph(id);このメソッドは何？これを起動すると、Listを作る
+		List<LifeGraph> graph = setGraph(id);
+		return ResponseEntity.ok(graph);
+	}
+
+//   これは１回書いたらいいのかな？
+//	@Autowired
+//	private NamedParameterJdbcTemplate jdbcTemplateNew;
+
+	private List<LifeGraph> setGraph(Integer id) {
+//		子グラフの中のparent_id, age, score, commentをparent_id = :idで照らし合わせて作る。
+//		まず、SQLに詰め込んどくイメージ
+		final String sql = "SELECT parent_id, age, score, comment FROM child_graphs WHERE parent_id = :id";
+//		 addValueを使うことで上のSQLの:idに下の右のidを入れている
+//		param:idに数字を入れる役割、
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+//		実行そのものはjdbcTemplate.queryでしてる。RowMapperは呪文
+		List<LifeGraph> result = jdbcTemplate.query(sql, param, new RowMapper<LifeGraph>() {
+//			RowMappers使ったらmapRow使う呪文
+			public LifeGraph mapRow(ResultSet rs, int rowNum) throws SQLException{
+//				LifeGraphに定義してある、コンストラクタに入ってるものを呼び出してくる。そして、それをresultに入れてる
+				return new LifeGraph(rs.getInt("parent_id"), rs.getInt("age"), rs.getInt("score"),rs.getString("comment"));
+			}
+//			resultの定義終了
+		});
+		return result;
+	}
 
 
 
