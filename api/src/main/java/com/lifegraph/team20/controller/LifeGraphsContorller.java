8b841ffcf088b3c30689ca1f3d910b29e2dfae2a
takahiro-kaji-1122 <takahiro.kaji@seattleconsulting.co.jp>
2020-05-21 20:16:24 +0900

    マージ

--- api/src/main/java/com/lifegraph/team20/controller/LifeGraphsContorller.java
+++ api/src/main/java/com/lifegraph/team20/controller/LifeGraphsContorller.java
@@ -1,31 +1,26 @@
 package com.lifegraph.team20.controller;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lifegraph.team20.models.ChildGraphData;
 import com.lifegraph.team20.models.LifeGraph;
-import com.lifegraph.team20.service.LifeGraphsService;
-import com.lifegraph.team20.models.LifeGraph;
 import com.lifegraph.team20.models.UserData;
 import com.lifegraph.team20.repository.LifeGraphsRepository;
 import com.lifegraph.team20.service.ChildService;
+import com.lifegraph.team20.service.LifeGraphsService;
 import com.lifegraph.team20.service.ParentService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -63,6 +58,30 @@ public class LifeGraphsContorller {
 		// サービスを呼ぶ
 		service.resister(data);
 		return ResponseEntity.ok("OK");
+	}
+
+	//登録編集APIの方でレスポンスを返すために使うかも
+//	private List<LifeGraph> setEditedGraph(long l) {
+//		//		子グラフの中のparent_id, age, score, commentをparent_id = :idで照らし合わせて作る。
+//		//		まず、SQLに詰め込んどくイメージ
+//		final String sql = "SELECT parent_id, age, score, comment FROM child_graphs WHERE parent_id = :id";
+//		//		 addValueを使うことで上のSQLの:idに下の右のidを入れている
+//		//		param:idに数字を入れる役割、
+//		SqlParameterSource param = new MapSqlParameterSource().addValue("id", l);
+//		//		実行そのものはjdbcTemplate.queryでしてる。RowMapperは呪文
+//		List<LifeGraph> result = jdbcTemplate.query(sql, param, new RowMapper<LifeGraph>() {
+//			//			RowMappers使ったらmapRow使う呪文
+//			public LifeGraph mapRow(ResultSet rs, int rowNum) throws SQLException {
+//				//				LifeGraphに定義してある、コンストラクタに入ってるものを呼び出してくる。そして、それをresultに入れてる
+//				return new LifeGraph(rs.getInt("parent_id"), rs.getInt("age"), rs.getInt("score"),
+//						rs.getString("comment"));
+//			}
+//			//			resultの定義終了
+//		});
+//		return result;
+//	}
+//	↑↑↑↑↑↑↑登録編集APIで使うかも
+
 	@Autowired
 	private LifeGraphsRepository lifeGraphsRepository;
 
@@ -98,27 +117,7 @@ public class LifeGraphsContorller {
 		List<LifeGraph> graph = lifeGraphsRepository.getGraph(id);
 		return ResponseEntity.ok(graph);
 	}
-//登録編集APIの方でレスポンスを返すために使うかも
-//	private List<LifeGraph> setEditedGraph(long l) {
-//		//		子グラフの中のparent_id, age, score, commentをparent_id = :idで照らし合わせて作る。
-//		//		まず、SQLに詰め込んどくイメージ
-//		final String sql = "SELECT parent_id, age, score, comment FROM child_graphs WHERE parent_id = :id";
-//		//		 addValueを使うことで上のSQLの:idに下の右のidを入れている
-//		//		param:idに数字を入れる役割、
-//		SqlParameterSource param = new MapSqlParameterSource().addValue("id", l);
-//		//		実行そのものはjdbcTemplate.queryでしてる。RowMapperは呪文
-//		List<LifeGraph> result = jdbcTemplate.query(sql, param, new RowMapper<LifeGraph>() {
-//			//			RowMappers使ったらmapRow使う呪文
-//			public LifeGraph mapRow(ResultSet rs, int rowNum) throws SQLException {
-//				//				LifeGraphに定義してある、コンストラクタに入ってるものを呼び出してくる。そして、それをresultに入れてる
-//				return new LifeGraph(rs.getInt("parent_id"), rs.getInt("age"), rs.getInt("score"),
-//						rs.getString("comment"));
-//			}
-//			//			resultの定義終了
-//		});
-//		return result;
-//	}
-//	↑↑↑↑↑↑↑登録編集APIで使うかも
+
 //
 //	private LifeGraphsRepository lifeGraphsRepository;
 //
