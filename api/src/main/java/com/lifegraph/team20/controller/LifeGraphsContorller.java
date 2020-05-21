75c5026c6a5b7d3110dea0a7245d6c5b26eb4835
eisuke-hayashi <email>
2020-05-21 20:01:30 +0900

    検索削除API

--- api/src/main/java/com/lifegraph/team20/controller/LifeGraphsContorller.java
+++ api/src/main/java/com/lifegraph/team20/controller/LifeGraphsContorller.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,7 +39,6 @@ public class LifeGraphsContorller {
     @Autowired
     private ParentService parentService;
 
-//  ToDo サービス名を適当なものに変更
     @DeleteMapping("/{parentId}")
     void deleteContent(@PathVariable("parentId") Integer parentId) {
 //    	子テーブルを消すメソッド
