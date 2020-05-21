6bd443fce907352479ff5dec05f8c9affe1cd6ce
naoueno <nao.ueno@seattleconsulting.co.jp>
2020-05-21 17:45:08 +0900

    0521

--- vue/.stylelintrc.js
+++ vue/.stylelintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  extends: 'stylelint-config-standard',
+  extends: 'lint-config-standard',
   plugins: ['stylelint-order', 'stylelint-scss'],
   rules: {
     'at-rule-no-unknown': null,
     'scss/at-rule-no-unknown': true,
     'order/properties-alphabetical-order': true,
     'no-empty-source': null,
-    'font-family-no-missing-generic-family-keyword': true,
+    'font-family-no-missing-generic-family-keyword': 0,
     'rule-empty-line-before': [
       'always',
       {
