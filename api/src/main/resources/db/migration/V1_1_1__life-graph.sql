2fc1b58679a590e2e9cff2f999d9db3a2d6e9b56
Manato-Okada <manato.okada@seattleconsulting.co.jp>
2020-05-25 10:58:16 +0900

    インサート文流した

---
+++ api/src/main/resources/db/migration/V1_1_1__life-graph.sql
@@ -0,0 +1,23 @@
+INSERT INTO `users` (
+`email`,        
+`username`,     
+`password`      
+) VALUE
+("manato@example.com","manato","$2a$10$EsxzbKMm1nFK6K/LVdVIxeDHhgWwo9GS5DSUj241yughANYD00MZS"),
+("ryuya@example.com","ryuya","$2a$10$pfys1Nmpx7g8yoZ3Xi4louBIA7hrUI0uSt4JDwW9xf/IK9SKuSdEm"),
+("eisuke@example.com","eisuke","$2a$10$5zIuKszAyHF3rK6PIKfy7ezRk7SIaA4i.qs.l46jOBhFllvBzYJ3u");
+
+INSERT INTO `parent_graphs` (
+`user_id`
+) VALUE
+(2),
+(1),
+(3);
+
+INSERT INTO `user_roles`(
+`user_id`,
+`role_id`
+) VALUE
+(1,1),
+(2,2),
+(3,3);
